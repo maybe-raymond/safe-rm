3ad7ebd830419fe12166e816e9587a4dec504b2b
maybe-ray <mugandiwaraymond@gmail.com>
2026-05-07 08:15:54 +0200

    About to do major re-write so that i do not need to manaully loop over the contents when deleting folders

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::ffi::OsStr;
 use std::fs;
 use std::io::ErrorKind;
 use std::path::PathBuf;
@@ -53,6 +54,25 @@ fn create_file_directory(folder_path: &PathBuf) -> Result<&PathBuf, std::io::Err
     }
 }
 
+fn process_folder_deletion(trash_path: &PathBuf, folder_name: &OsStr, folder_path: &PathBuf) {
+    // creating a folder in trash path to push all the new items
+    match create_file_directory(&trash_path.join(folder_name)) {
+        Ok(path) => {
+            //getting files in the folder
+            // Will fail if the path is a file
+            let folder_contents = folder_path
+                .read_dir()
+                .expect("Could not read the contents of folder");
+
+            delete_files_in_path(folder_contents, &path);
+            // deleting folder
+            fs::remove_dir(folder_path)
+                .expect("Failed to delete folder. Please check your permissions");
+        }
+        Err(e) => eprintln!("{}", e),
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -69,7 +89,9 @@ fn main() {
         "-h" => println!(
             "Safe-rm is a safe alternative to rm command. It outputs all removed files to the Trash folder."
         ),
+
         "-r" => {
+            // meeant for folders
             let (_, folder_list) = args.split_at(2);
 
             if folder_list.len() < 2 {
@@ -81,26 +103,10 @@ fn main() {
             for folder in folders {
                 match folder {
                     Ok(folder_path) => {
-                        let folder_name = folder_path
-                            .file_name()
-                            .expect("selected directory does not have a name");
-
-                        // creating a folder in trash path to push all the new items
-                        match create_file_directory(&trash_location.join(folder_name)) {
-                            Ok(path) => {
-                                //getting files in the folder
-                                // Will fail if the path is a file
-                                let folder_contents = folder_path
-                                    .read_dir()
-                                    .expect("Could not read the contents of folder");
-
-                                delete_files_in_path(folder_contents, &path);
-                                // deleting folder
-                                fs::remove_dir(folder_path).expect(
-                                    "Failed to delete folder. Please check your permissions",
-                                );
-                            }
-                            Err(e) => eprintln!("{}", e),
+                        if let Some(folder_name) = folder_path.file_name() {
+                            process_folder_deletion(&trash_location, folder_name, &folder_path);
+                        } else {
+                            eprintln!("selected directory does not have a name");
                         }
                     }
                     Err(e) => eprint!("Could not resolve path with error: {}", e),
@@ -108,16 +114,24 @@ fn main() {
             }
         }
         _ => {
-            //Getting the absolute path for all the files
-            //Already checked if the len of the args are bigger than one
+            // Already checked if the len of the args are bigger than one
             let (_, files) = args.split_at(1);
 
+            //Getting the absolute path for all the files
             let all_files = files.into_iter().map(fs::canonicalize);
 
             // looping through all the files and moving them to Trash one by one
             for file_path in all_files {
                 match file_path {
-                    Ok(path) => move_to_trash(&trash_location, path),
+                    Ok(path) => {
+                        // This might not be nessary since move to trah moved the whole project including the subfolders
+                        // Might just need to expirement with it
+                        if path.is_file() {
+                            move_to_trash(&trash_location, path)
+                        } else {
+                            println!("{:?} is a direcory, please use the -r command", path)
+                        }
+                    }
                     Err(e) => eprintln!("Program exited with error: {:}", e),
                 }
             }
