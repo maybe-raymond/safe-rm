323e6813f99378a9741d2a3f13922e1eba0ad852
maybe-ray <mugandiwaraymond@gmail.com>
2026-06-10 16:47:40 +0200

    Created the tests for the file. All that is left is to Finish up the final test

--- safe-rm/tests/common/mod.rs
+++ safe-rm/tests/common/mod.rs
@@ -1,11 +1,30 @@
 use std::fs;
 use std::path::PathBuf;
 
+// once created auto deletes everything in the folder
+pub struct TempFolder {
+    pub path: PathBuf,
+}
+
+// delete the folder and all it's files once it is done
+impl Drop for TempFolder {
+    fn drop(&mut self) {
+        match self.path.try_exists() {
+            Err(_) => println!("File already deleted"),
+            Ok(false) => println!("File does not exist"),
+            Ok(true) => {
+                let _ = fs::remove_dir_all(&self.path);
+            }
+        };
+    }
+}
+
 // creating a temporary trash bin
-pub fn setup() -> Option<PathBuf> {
-    let path = "trash";
-    match fs::create_dir(path) {
-        Ok(_) => Some(PathBuf::from(path)),
+pub fn setup(folder_name: &str) -> Option<TempFolder> {
+    match fs::create_dir(folder_name) {
+        Ok(_) => Some(TempFolder {
+            path: PathBuf::from(folder_name),
+        }),
         Err(e) => {
             println!("{:?}", e);
             None
--- safe-rm/tests/integration_safe_rm_test.rs
+++ safe-rm/tests/integration_safe_rm_test.rs
@@ -3,35 +3,95 @@ mod common;
 use std::fs;
 use std::path::Path;
 
+use safe_rm::process_content_for_deletion;
+
 #[test]
 fn delete_one_file() {
     // deletes one file
     let file_name = "code_base.py";
-    let trash_path = common::setup().unwrap();
+    let trash_path = common::setup("trash_delete_one_file").unwrap();
     let _file = fs::File::create(file_name).unwrap();
 
     let new_path = Path::new(file_name);
-    safe_rm::process_content_for_deletion(trash_path.as_path(), &[String::from(file_name)]);
+    safe_rm::process_content_for_deletion(trash_path.path.as_path(), &[String::from(file_name)]);
 
-    assert!(!new_path.exists())
+    assert!(!new_path.exists() && trash_path.path.join(file_name).exists())
 }
 
 #[test]
 fn delete_one_folder() {
-    // deletes one file
-}
+    // deletes a folder and all it's contents/
+    // checks to see if temp folder is in trash path
+    // checks to see if the deleted files are in the new trash folder
+    let trash_path = common::setup("trash_delete_one_folder").unwrap();
+    let temp_folder = common::setup("temp_folder").unwrap();
 
-#[test]
-fn delete_multiple_folders() {
-    // deletes one file
+    // create 5 files in temp directory
+    (0..5).for_each(|x| {
+        fs::File::create(temp_folder.path.join(format!("temp_{x}"))).unwrap();
+    });
+
+    safe_rm::process_content_for_deletion(
+        trash_path.path.as_path(),
+        &[String::from("temp_folder")],
+    );
+
+    // get the amount of files in the trash
+    let trash_amount = fs::read_dir(&trash_path.path).unwrap().count();
+    let files_amount = fs::read_dir(trash_path.path.join("temp_folder"))
+        .unwrap()
+        .count();
+
+    // check if the folder still exists and trash has 4 files
+    let exists = temp_folder.path.try_exists().unwrap_or(false);
+    assert!(!exists && trash_amount == 1 && files_amount == 5)
 }
 
 #[test]
 fn delete_multiple_files() {
     // deletes one file
+    let trash_path = common::setup("trash_delete_mut_files").unwrap();
+
+    // create 5 files in temp directory
+    let file_names = (0..5).map(|x| format!("temp_{x}"));
+
+    // creates Files
+    file_names.clone().for_each(|i| {
+        fs::File::create(i).unwrap();
+    });
+
+    let file_list = &file_names.clone().collect::<Vec<_>>();
+    process_content_for_deletion(trash_path.path.as_path(), file_list);
+
+    assert!(file_names.clone().all(|x| !fs::exists(x).unwrap_or(false)));
 }
 
 #[test]
-fn try_delete_file_that_does_exist() {
+fn delete_multiple_folders() {
     // deletes one file
+    todo!()
 }
+
+/*
+* Can not do tests on deleting a file or folder that does not exist since
+* process_content_for_deletion does not return an Result type
+*
+*
+#[test]
+#[should_panic]
+fn try_delete_file_that_does_not_exist() {
+    // tries to delete a file that does not exist
+    let file_name = "code_base.py";
+    let trash_path = common::setup("trash_delete_one_file").unwrap();
+
+    safe_rm::process_content_for_deletion(trash_path.path.as_path(), &[String::from(file_name)]);
+}
+
+#[test]
+#[should_panic]
+fn try_delete_folder_that_does_not_exist() {
+    // tries to delete a folder that does not exist
+    todo!()
+}
+
+*/
